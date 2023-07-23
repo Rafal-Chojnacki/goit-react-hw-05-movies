732ba29f09d25ff98e092a5275b4b875a3a8dff8
Rafał Chojnacki <r.chojnacki2@o2.pl>
2023-07-24 01:10:46 +0200

    Added reviews in component Reviews

--- src/pages/Reviews/index.jsx
+++ src/pages/Reviews/index.jsx
@@ -1,13 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useOutletContext } from 'react-router';
+import css from './reviews.module.css';
+import { BsPersonCircle } from 'react-icons/bs'; // Assuming you have imported the BsPersonCircle icon from the react-icons library.
 
 export const Reviews = () => {
+  const [movieReviews, setMovieReviews] = useState([]);
+  const id = useOutletContext();
+
+  useEffect(() => {
+    const fetchMovieReviews = async () => {
+      try {
+        const response = await fetch(
+          `https://api.themoviedb.org/3/movie/${id}/reviews?api_key=e7c806d7ce9bbdf1ef93bebcabbfe0f1&language=en-US`
+        );
+        const data = await response.json();
+        setMovieReviews(data.results);
+        console.log(data);
+      } catch (error) {
+        console.error('Error fetching movie reviews:', error);
+      }
+    };
+
+    fetchMovieReviews();
+  }, [id]);
+
   return (
     <div>
-      <h2>Review</h2>
-      {/* Add review information here */}
-      <p>Review 1</p>
-      <p>Review 2</p>
-      {/* Add more review details as needed */}
+      <div>
+        <h2>Reviews</h2>
+      </div>
+      <div className={css.reviewsBox}>
+        {movieReviews.map((review) => (
+          <div key={review.id}>
+            <ul className={css.listOfReviews}>
+              <li className={css.reviewElement}>
+                <p className={css.authorName}>{review.author}</p>
+                <p className={css.reviewContent}>{review.content}</p>
+              </li>
+            </ul>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
